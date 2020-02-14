b610b1c282648072955c8f8c3f42162312ae6cd7
Ian Nguyen <iannguyen172@gmail.com>
2020-02-14 22:35:01 +1100

    To-do list functionality

--- app.js
+++ app.js
@@ -6,11 +6,42 @@ const app = express();
 
 //set up middlewares
 app.set('view engine', 'ejs');
-app.use(bodyParser.encodedurl({encoded:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+//variables
+let items = [];
+let workItems = [];
 
 //routing
 app.get("/", function(req,res){
-  res.send("hello");
+  let options = {
+    weekday:'long',
+    day:'numeric',
+    month:'long'};
+  let date = new Date().toLocaleDateString("en-AU", options);
+
+  res.render("list",{listTitle:date, newListItems:items});
+});
+
+app.post("/",function(req,res){
+
+  if(req.body.list == "Work"){
+    workItems.push(req.body.newItem);
+    res.redirect("/work");
+  }else{
+    items.push(req.body.newItem);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work",function(req,res){
+  res.render("list",{listTitle:"Work", newListItems: workItems});
+});
+
+app.get("/about",function(req,res){
+  res.render("about");
 });
 
 app.listen(3000,function(){
